fb2f10b5fef7de3c148d5c91daabb8e9d78a89b8
Mayar Ahmed <mma121@pitt.edu>
2026-08-13 11:37:34 -0400

    chore: adding the license notice to files

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 University of Pittsburgh — Of the Commonwealth System of Higher Education
+# Source: https://github.com/durrantlab/lignova
+
 """Generate the code reference pages."""
 
 import os
--- hooks/launchy.py
+++ hooks/launchy.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 University of Pittsburgh — Of the Commonwealth System of Higher Education
+# Source: https://github.com/durrantlab/lignova
+
 import os
 
 from bs4 import BeautifulSoup
