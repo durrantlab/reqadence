3bf87b92f6d6b924faa3349c831aecc5aa9d6947
Mayar Ahmed <mma121@pitt.edu>
2026-08-13 12:09:02 -0400

    chore: adding license notice to files

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 University of Pittsburgh — Of the Commonwealth System of Higher Education
+# Source: https://github.com/durrantlab/reqadence
+
+
 def test_example():
     pass
