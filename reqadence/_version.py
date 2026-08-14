8b81f73cfb14f47dc30f7e19e56b880577eac5df
Mayar Ahmed <mma121@pitt.edu>
2026-08-13 20:54:33 -0400

    chore: fix license

--- reqadence/_version.py
+++ reqadence/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.1.dev7+g4477ca8d5.d20260813190907"
-__version_tuple__ = version_tuple = (0, 1, "dev7", "g4477ca8d5.d20260813190907")
+__version__ = version = '0.1.dev13+g392dc4836.d20260814005236'
+__version_tuple__ = version_tuple = (0, 1, 'dev13', 'g392dc4836.d20260814005236')
 
-__commit_id__ = commit_id = "g4477ca8d5"
+__commit_id__ = commit_id = 'g392dc4836'
